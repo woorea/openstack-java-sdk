7e32cef3a05e6373cd1c0abb2415e7555cb4a811
Justin Santa Barbara <justin@fathomdb.com>
2012-02-21 11:18:57 -0800

    We can now disable glance tests in the integration tests (it is not always accessible)

--- openstack-api/src/test/java/org/openstack/client/OpenstackTestContext.java
+++ openstack-api/src/test/java/org/openstack/client/OpenstackTestContext.java
@@ -5,14 +5,13 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
-import javax.ws.rs.core.MediaType;
-
 import org.openstack.client.common.OpenstackSession;
 import org.openstack.utils.Io;
 
 public class OpenstackTestContext {
 
 	public OpenstackSession session;
+	private boolean glanceEnabled;
 
 	public OpenstackSession connect(String url, OpenstackCredentials credentials, String format, boolean verbose) {
 		session = new OpenstackSession();
@@ -23,21 +22,18 @@ public class OpenstackTestContext {
 		if (format != null) {
 			if (format.equals("json")) {
 				session.with(OpenstackSession.Feature.FORCE_JSON);
-			}
-			else if (format.equals("xml")) {
+			} else if (format.equals("xml")) {
 				session.with(OpenstackSession.Feature.FORCE_XML);
 			} else {
 				throw new IllegalArgumentException("Unknown format: " + format);
 			}
 		}
-		
+
 		session.authenticate(url, credentials);
 		return session;
 	}
 
 	public static OpenstackTestContext buildFromProperties() {
-		OpenstackTestContext context = new OpenstackTestContext();
-
 		Properties properties = System.getProperties();
 
 		String configPath = properties.getProperty("openstack.config", null);
@@ -66,9 +62,18 @@ public class OpenstackTestContext {
 		String tenant = properties.getProperty("openstack.auth.tenant", "demo");
 
 		String format = properties.getProperty("openstack.format", null);
-		
+
+		boolean glanceEnabled = Boolean.parseBoolean(properties.getProperty("openstack.glance", "true"));
+
+		OpenstackTestContext context = new OpenstackTestContext();
+		context.glanceEnabled = glanceEnabled;
+
 		OpenstackCredentials credentials = new OpenstackCredentials(username, secret, tenant);
 		context.connect(url, credentials, format, verbose);
 		return context;
+	}
+
+	public boolean isGlanceEnabled() {
+		return glanceEnabled;
 	}
 }
--- openstack-api/src/test/java/org/openstack/client/image/GlanceIntegrationTest.java
+++ openstack-api/src/test/java/org/openstack/client/image/GlanceIntegrationTest.java
@@ -3,6 +3,7 @@ package org.openstack.client.image;
 import org.openstack.client.AbstractOpenStackTest;
 import org.openstack.client.common.OpenstackImageClient;
 import org.openstack.client.utils.RandomUtil;
+import org.testng.SkipException;
 
 public abstract class GlanceIntegrationTest extends AbstractOpenStackTest {
 	protected RandomUtil random = new RandomUtil();
@@ -10,8 +11,10 @@ public abstract class GlanceIntegrationTest extends AbstractOpenStackTest {
 	protected OpenstackImageClient getImageClient() {
 		return context.session.getImageClient();
 	}
-	
+
 	protected void skipIfNoGlance() {
-		// throw new SkipException("Skipping because glance not present");
+		if (!context.haveGlance()) {
+			throw new SkipException("Skipping because glance not present / accessible");
+		}
 	}
 }
