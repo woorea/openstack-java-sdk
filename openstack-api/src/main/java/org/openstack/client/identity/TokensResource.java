da52085df1860fa4b710ff19082fa36dc40dd26d
Justin Santa Barbara <justin@fathomdb.com>
2012-02-21 23:55:35 -0800

    FindBugs fixes: toHex can just use Hex class; specify encoding with String.getBytes

--- openstack-api/src/main/java/org/openstack/client/identity/TokensResource.java
+++ openstack-api/src/main/java/org/openstack/client/identity/TokensResource.java
@@ -7,6 +7,8 @@ import org.apache.commons.codec.binary.Hex;
 import org.openstack.model.identity.Access;
 import org.openstack.model.identity.Authentication;
 
+import com.google.common.base.Charsets;
+
 public class TokensResource extends IdentityResourceBase {
 
     public Access authenticate(Authentication authentication) {
@@ -16,22 +18,11 @@ public class TokensResource extends IdentityResourceBase {
     	return resource().post(Access.class, authentication);
     }
 
-    private String toHex(byte[] digest) {
-        String hash = "";
-        for (byte aux : digest) {
-            int b = aux & 0xff; // Hace un cast del byte a hexadecimal
-            if (Integer.toHexString(b).length() == 1)
-                hash += "0";
-            hash += Integer.toHexString(b);
-        }
-        return hash;
-    }
-
     private String sha512(String text) {
         try {
             MessageDigest md = MessageDigest.getInstance("SHA-512");
             md.reset();
-            md.update(text.getBytes());
+            md.update(text.getBytes(Charsets.UTF_8));
             byte[] digest = md.digest();
             return Hex.encodeHexString(digest);
             // return toHex(digest);
