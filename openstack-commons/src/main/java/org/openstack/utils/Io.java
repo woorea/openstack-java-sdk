71d6a0ddd2cda9b3ed41f01190593576fcd8c39c
Justin Santa Barbara <justin@fathomdb.com>
2012-02-23 15:43:29 -0800

    A few more Io Utils - readAll

--- openstack-commons/src/main/java/org/openstack/utils/Io.java
+++ openstack-commons/src/main/java/org/openstack/utils/Io.java
@@ -1,7 +1,13 @@
 package org.openstack.utils;
 
+import java.io.BufferedReader;
 import java.io.Closeable;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -12,20 +18,50 @@ import java.util.logging.Logger;
  * 
  */
 public class Io {
-    static final Logger log = Logger.getLogger(Io.class.getName());
+	static final Logger log = Logger.getLogger(Io.class.getName());
 
-    public static void safeClose(Closeable closeable) {
-        if (closeable == null) {
-            return;
-        }
-        try {
-            closeable.close();
-        } catch (IOException e) {
-            logError("Ignoring unexpected error closing item", e);
-        }
-    }
+	public static void safeClose(Closeable closeable) {
+		if (closeable == null) {
+			return;
+		}
+		try {
+			closeable.close();
+		} catch (IOException e) {
+			logError("Ignoring unexpected error closing item", e);
+		}
+	}
 
-    static final void logError(String message, Throwable e) {
-        log.log(Level.SEVERE, message, e);
-    }
+	static final void logError(String message, Throwable e) {
+		log.log(Level.SEVERE, message, e);
+	}
+
+	public static String readAll(Reader in) throws IOException {
+		StringBuilder contents = new StringBuilder();
+
+		char[] buffer = new char[8192];
+		while (true) {
+			int readCount = in.read(buffer);
+			if (readCount == -1) {
+				break;
+			}
+			contents.append(buffer, 0, readCount);
+		}
+
+		return contents.toString();
+	}
+
+	public static String readAll(InputStream inputStream) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(
+				inputStream));
+		return readAll(in);
+	}
+
+	public static String readAll(File file) throws IOException {
+		BufferedReader in = new BufferedReader(new FileReader(file));
+		try {
+			return readAll(in);
+		} finally {
+			safeClose(in);
+		}
+	}
 }
