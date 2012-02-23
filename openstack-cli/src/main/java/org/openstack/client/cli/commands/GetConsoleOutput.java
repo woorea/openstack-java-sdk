c490a8998c38c2c47281cfad46860d458c577437
Justin Santa Barbara <justin@fathomdb.com>
2012-02-23 15:43:58 -0800

    Added get-consoleoutput CLI command

---
+++ openstack-cli/src/main/java/org/openstack/client/cli/commands/GetConsoleOutput.java
@@ -0,0 +1,26 @@
+package org.openstack.client.cli.commands;
+
+import org.kohsuke.args4j.Argument;
+import org.openstack.client.cli.model.InstanceName;
+import org.openstack.client.common.OpenstackComputeClient;
+
+public class GetConsoleOutput extends OpenstackCliCommandRunnerBase {
+	@Argument(index = 0)
+	public InstanceName instanceName;
+
+	public GetConsoleOutput() {
+		super("get", "consoleoutput");
+	}
+
+	@Override
+	public Object runCommand() throws Exception {
+		OpenstackComputeClient compute = getComputeClient();
+
+		String instanceId = instanceName.findInstanceId(getContext());
+
+		String output = compute.root().servers().server(instanceId).getConsoleOutput(null);
+
+		return output;
+	}
+
+}
