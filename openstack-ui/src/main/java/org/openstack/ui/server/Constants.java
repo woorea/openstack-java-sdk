c5febca2145c19dc761d6d565cd4f7a293e44569
woorea <luis@woorea.es>
2012-03-16 04:41:06 +0100

    bf

--- openstack-ui/src/main/java/org/openstack/ui/server/Constants.java
+++ openstack-ui/src/main/java/org/openstack/ui/server/Constants.java
@@ -1,5 +1,10 @@
 package org.openstack.ui.server;
 
+/**
+ * 
+ * @author woorea
+ *
+ */
 public class Constants {
 
 	public static final String OPENSTACK_SESSION = "org.openstack.ui.session.OpenStackSession";
