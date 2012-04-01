ea2f579271b300e2221f640cb8d0536bf0e9aa40
woorea <luis@woorea.es>
2012-04-01 07:32:58 +0200

    bf

--- openstack-api/src/main/java/org/openstack/api/compute/ServerActionResource.java
+++ openstack-api/src/main/java/org/openstack/api/compute/ServerActionResource.java
@@ -5,9 +5,7 @@ import javax.ws.rs.client.Target;
 import javax.ws.rs.core.MediaType;
 
 import org.openstack.api.common.Resource;
-import org.openstack.model.compute.Server;
 import org.openstack.model.compute.ServerAction;
-import org.openstack.model.compute.nova.NovaServer;
 
 public class ServerActionResource extends Resource {
 
