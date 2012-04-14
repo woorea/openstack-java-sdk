10d36f8c111d0f34ef30ab1466b084d0ae11cc7f
woorea <luis@woorea.es>
2012-04-14 07:23:48 +0200

    bf

--- src/main/java/org/openstack/model/compute/SecurityGroup.java
+++ src/main/java/org/openstack/model/compute/SecurityGroup.java
@@ -4,24 +4,24 @@ import java.util.List;
 
 public interface SecurityGroup {
 
-	public abstract void setRules(List<SecurityGroupRule> rules);
+	void setRules(List<SecurityGroupRule> rules);
 
-	public abstract List<SecurityGroupRule> getRules();
+	List<SecurityGroupRule> getRules();
 
-	public abstract void setDescription(String description);
+	void setDescription(String description);
 
-	public abstract String getDescription();
+	String getDescription();
 
-	public abstract void setName(String name);
+	void setName(String name);
 
-	public abstract String getName();
+	String getName();
 
-	public abstract void setTenantId(String tenantId);
+	void setTenantId(String tenantId);
 
-	public abstract String getTenantId();
+	String getTenantId();
 
-	public abstract void setId(Integer id);
+	void setId(Integer id);
 
-	public abstract Integer getId();
+	Integer getId();
 
 }
