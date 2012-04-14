e2f5d5d889826957d68ef9edec627b121bf221d5
woorea <luis@woorea.es>
2012-04-14 02:04:10 +0200

    bf

--- src/main/java/org/openstack/model/identity/Endpoint.java
+++ src/main/java/org/openstack/model/identity/Endpoint.java
@@ -2,16 +2,30 @@ package org.openstack.model.identity;
 
 public interface Endpoint {
 
+	String getId();
+	
+	void setId(String id);
+	
 	String getRegion();
+	
+	void setRegion(String region);
 
 	String getServiceId();
+	
+	void setServiceId(String serviceId);
 
 	String getPublicURL();
+	
+	void setPublicURL(String publicURL);
 
 	String getAdminURL();
+	
+	void setAdminURL(String adminURL);
 
 	String getInternalURL();
+	
+	void setInternalURL(String internalURL);
 
-	public abstract String getId();
+	
 
 }
