e741b7f66b645393be6b22cc32449bc09048a533
woorea <luis@woorea.es>
2012-05-30 01:44:27 +0200

    case bug 2

---
+++ src/main/java/org/openstack/model/exceptions/OpenStackException.java
@@ -0,0 +1,25 @@
+package org.openstack.model.exceptions;
+
+
+public class OpenStackException extends RuntimeException {
+	
+	private int status;
+
+	public OpenStackException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public OpenStackException(int status, String message, Throwable cause) {
+		super(message, cause);
+		this.status = status;
+	}
+
+	public OpenStackException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+}
