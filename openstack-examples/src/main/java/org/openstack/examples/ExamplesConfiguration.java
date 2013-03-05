fe7db942b79d5ea463578dacdac7071fcceaf605
woorea <luis@woorea.es>
2013-03-05 09:35:46 +0100

    xxx

--- openstack-examples/src/main/java/org/openstack/examples/ExamplesConfiguration.java
+++ openstack-examples/src/main/java/org/openstack/examples/ExamplesConfiguration.java
@@ -2,12 +2,12 @@ package org.openstack.examples;
 
 public class ExamplesConfiguration {
 
-	public static final String KEYSTONE_AUTH_URL = "";
+	public static final String KEYSTONE_AUTH_URL = "http://identity.stacksherpa.org/v2.0";
 	
 	public static final String KEYSTONE_USERNAME = "admin";
 	
 	public static final String KEYSTONE_PASSWORD = "secret0";
 	
-	public static final String NOVA_ENDPOINT = "";
+	public static final String NOVA_ENDPOINT = "http://compute.stacksherpa.org/v2";
 	
 }
