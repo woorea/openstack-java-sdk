263b928c93ebfe3b7a05c2144b63f556bbd90eff
Federico Simoncelli <fsimonce@redhat.com>
2013-04-22 20:36:37 +0200

    access: move out the token implementation
    Token shouldn't be a specific model inside Access. The code has been
    moved to a separate class.
    
    Signed-off-by: Federico Simoncelli <fsimonce@redhat.com>

--- keystone-model/src/main/java/org/openstack/keystone/model/Access.java
+++ keystone-model/src/main/java/org/openstack/keystone/model/Access.java
@@ -1,7 +1,6 @@
 package org.openstack.keystone.model;
 
 import java.io.Serializable;
-import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
 
@@ -12,55 +11,6 @@ import org.codehaus.jackson.map.annotate.JsonRootName;
 @JsonRootName("access")
 public class Access implements Serializable {
 
-	public static final class Token {
-		
-		private String id;
-
-		private Calendar issued_at;
-		
-		private Calendar expires;
-		
-		private Tenant tenant;
-
-		/**
-		 * @return the id
-		 */
-		public String getId() {
-			return id;
-		}
-
-    /**
-     * @return the issued_at
-     */
-    public Calendar getIssued_at() {
-      return issued_at;
-    }
-
-		/**
-		 * @return the expires
-		 */
-		public Calendar getExpires() {
-			return expires;
-		}
-
-		/**
-		 * @return the tenant
-		 */
-		public Tenant getTenant() {
-			return tenant;
-		}
-
-		/* (non-Javadoc)
-		 * @see java.lang.Object#toString()
-		 */
-		@Override
-		public String toString() {
-			return "Token [id=" + id + ", Issued_at=" + issued_at + ", expires=" + expires + ", tenant="
-              + tenant + "]";
-		}
-		
-	}
-	
 	public static final class Service {
 		
 		@JsonIgnoreProperties(ignoreUnknown=true)
