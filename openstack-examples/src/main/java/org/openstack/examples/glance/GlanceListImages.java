5ddc650944e178b9584131c8c39cbabf98cee924
Federico Simoncelli <fsimonce@redhat.com>
2013-04-24 16:58:00 +0200

    token: add the KeystoneTokenProvider
    The KeystoneTokenProvider has been introduced as part of the new
    OpenStackTokenProvider mechanism to provide dynamic tokens.
    Once the KeystoneTokenProvider has been configured with endpoint,
    username and password it's able to generate dynamic tokens (coping
    with expiration) using the providers (per-tenant) generated by
    getProviderByTenant.
    
    Example of use:
    
      KeystoneTokenProvider keystone = new KeystoneTokenProvider(
              keystoneEndpoint, username, password);
    
      GlanceClient glance = new GlanceClient(glanceEndpoint);
      glance.setTokenProvider(keystone.getProviderByTenant(tenantName));
    
      Images images = glance.execute(new ListImages(false));
    
    Signed-off-by: Federico Simoncelli <fsimonce@redhat.com>

--- openstack-examples/src/main/java/org/openstack/examples/glance/GlanceListImages.java
+++ openstack-examples/src/main/java/org/openstack/examples/glance/GlanceListImages.java
@@ -1,16 +1,14 @@
 package org.openstack.examples.glance;
 
-import org.openstack.base.client.OpenStackSimpleTokenProvider;
 import org.openstack.examples.ExamplesConfiguration;
 import org.openstack.glance.GlanceClient;
 import org.openstack.glance.api.ListImages;
 import org.openstack.glance.model.Image;
 import org.openstack.glance.model.Images;
-import org.openstack.keystone.KeystoneClient;
-import org.openstack.keystone.api.Authenticate;
 import org.openstack.keystone.model.Access;
 import org.openstack.keystone.model.Access.Service;
 import org.openstack.keystone.model.Access.Service.Endpoint;
+import org.openstack.keystone.utils.KeystoneTokenProvider;
 
 public class GlanceListImages {
 
@@ -18,14 +16,12 @@ public class GlanceListImages {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		KeystoneClient keystone = new KeystoneClient(
-				ExamplesConfiguration.KEYSTONE_AUTH_URL);
+		KeystoneTokenProvider keystone = new KeystoneTokenProvider(
+				ExamplesConfiguration.KEYSTONE_AUTH_URL,
+				ExamplesConfiguration.KEYSTONE_USERNAME,
+				ExamplesConfiguration.KEYSTONE_PASSWORD);
 
-		Access access = keystone.execute(
-				Authenticate.withPasswordCredentials(
-						ExamplesConfiguration.KEYSTONE_USERNAME,
-						ExamplesConfiguration.KEYSTONE_PASSWORD)
-				.withTenantName(ExamplesConfiguration.TENANT_NAME));
+		Access access = keystone.getAccessByTenant(ExamplesConfiguration.TENANT_NAME);
 
 		Service glanceService = null;
 
@@ -42,7 +38,8 @@ public class GlanceListImages {
 
 		for (Endpoint endpoint : glanceService.getEndpoints()) {
 			GlanceClient glance = new GlanceClient(endpoint.getPublicURL() + "/v1");
-			glance.setTokenProvider(new OpenStackSimpleTokenProvider(access.getToken().getId()));
+			glance.setTokenProvider(keystone
+					.getProviderByTenant(ExamplesConfiguration.TENANT_NAME));
 
 			Images images = glance.execute(new ListImages(false));
 
