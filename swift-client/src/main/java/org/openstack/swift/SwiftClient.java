bdb0f7d481c025056fc4227f8c8cea05bc81d34b
efoncubierta <ezequiel.foncubierta@ixxus.com>
2013-10-10 16:32:46 +0100

    Single endpoint instance
    For each command is executed through SwiftClient a new listener is
    created in OpenStack.CLIENT.listeners . In order to avoid filling the
    memory with the same listener again and again, I have created the
    endpoint just once for each SwiftClient instance.

--- swift-client/src/main/java/org/openstack/swift/SwiftClient.java
+++ swift-client/src/main/java/org/openstack/swift/SwiftClient.java
@@ -7,15 +7,20 @@ import org.openstack.common.client.AbstractOpenStackClient;
 
 public class SwiftClient extends AbstractOpenStackClient {
 	
+	private WebTarget endpoint = null;
+	
 	public SwiftClient(String endpointURL, String token) {
 		super(endpointURL, token);
 	}
 
 	public <R> R execute(SwiftCommand<R> command) {
-		WebTarget endpoint = OpenStack.CLIENT.target(endpointURL);
-		if(token != null) {
-			endpoint.register(tokenFilter);
+		if(endpoint == null) {
+			endpoint = OpenStack.CLIENT.target(endpointURL);
+			if(token != null) {
+				endpoint.register(tokenFilter);
+			}
 		}
+		
 		return command.execute(endpoint);
 	}
 
