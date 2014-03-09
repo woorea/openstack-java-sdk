9b795e2e149681ddeb7c30c46bd06c8f85e33462
Daniel Kuffner <dkuffner@chilicat.net>
2014-03-09 13:28:55 +0100

    fixed example

--- openstack-examples/src/main/java/com/woorea/openstack/examples/heat/HeatListStacks.java
+++ openstack-examples/src/main/java/com/woorea/openstack/examples/heat/HeatListStacks.java
@@ -5,15 +5,14 @@ import com.woorea.openstack.heat.Heat;
 import com.woorea.openstack.heat.model.CreateStackParam;
 import com.woorea.openstack.heat.model.Stack;
 import com.woorea.openstack.keystone.model.Access;
-import com.woorea.openstack.keystone.model.Access.Service;
-import com.woorea.openstack.keystone.model.Access.Service.Endpoint;
 import com.woorea.openstack.keystone.utils.KeystoneTokenProvider;
+import com.woorea.openstack.keystone.utils.KeystoneUtils;
 
 import java.util.Collections;
 
 public class HeatListStacks {
 
-    private static  String template = "{\n" +
+    private static String TEMPLATE = "{\n" +
             "    \"HeatTemplateFormatVersion\": \"2012-12-12\",\n" +
             "    \"Parameters\": {},\n" +
             "    \"Mappings\": {},\n" +
@@ -33,60 +32,41 @@ public class HeatListStacks {
      */
     public static void main(String[] args) throws InterruptedException {
         KeystoneTokenProvider keystone = new KeystoneTokenProvider(
-                "http://10.129.184.5:35357/v2.0/",
-                "admin",
-                "aa2f03a2c4c2414c");
+                ExamplesConfiguration.KEYSTONE_ENDPOINT,
+                ExamplesConfiguration.KEYSTONE_USERNAME,
+                ExamplesConfiguration.KEYSTONE_PASSWORD
+        );
 
         Access access = keystone.getAccessByTenant(ExamplesConfiguration.TENANT_NAME);
 
-        Service heatService = getService(access);
-
-        for (Endpoint endpoint : heatService.getEndpoints()) {
-
-            Heat heat = new Heat(endpoint.getPublicURL());
-            heat.setTokenProvider(keystone
-                    .getProviderByTenant(ExamplesConfiguration.TENANT_NAME));
-
-            CreateStackParam param = new CreateStackParam();
-            param.setStackName("helloWorld");
-            param.setTimeoutMinutes(1);
-            param.setParameters(Collections.<String, String>emptyMap());
-            param.setTemplate(template);
-
-            System.out.printf("Create: " + heat.getStacks().create(param).execute());
-            Thread.sleep(3000);
+        String endpointURL = KeystoneUtils.findEndpointURL(access.getServiceCatalog(), "orchestration", null, "public");
 
 
-            for (Stack s : heat.getStacks().list().execute()) {
-                System.out.println(s.getDescription());
-                System.out.println(s.getId());
-                System.out.println(s.getStackName());
-                System.out.println(s.getStackStatus());
-                System.out.println(s.getCreationTime());
-                System.out.println(s.getUpdatedTime());
-                System.out.println(s.getLinks());
+        Heat heat = new Heat(endpointURL);
+        heat.setTokenProvider(keystone
+                .getProviderByTenant(ExamplesConfiguration.TENANT_NAME));
 
-                System.out.println(heat.getStacks().byName(s.getStackName()).execute());
+        CreateStackParam param = new CreateStackParam();
+        param.setStackName("helloWorld");
+        param.setTimeoutMinutes(1);
+        param.setParameters(Collections.<String, String>emptyMap());
+        param.setTemplate(TEMPLATE);
 
-                System.out.println("Delete: " + heat.getStacks().deleteByName(s.getStackName()).execute());
+        System.out.printf("Create: " + heat.getStacks().create(param).execute());
+        Thread.sleep(3000);
 
-            }
+        for (Stack s : heat.getStacks().list().execute()) {
+            System.out.println(s.getDescription());
+            System.out.println(s.getId());
+            System.out.println(s.getStackName());
+            System.out.println(s.getStackStatus());
+            System.out.println(s.getCreationTime());
+            System.out.println(s.getUpdatedTime());
+            System.out.println(s.getLinks());
+
+            System.out.println(heat.getStacks().byName(s.getStackName()).execute());
+
+
         }
     }
-
-    private static Service getService(Access access) {
-        Service heatService = null;
-        for (Service service : access.getServiceCatalog()) {
-            if (service.getType().equals("orchestration")) {
-                heatService = service;
-                break;
-            }
-        }
-
-        if (heatService == null) {
-            throw new RuntimeException("Glance service not found");
-        }
-        return heatService;
-    }
-
 }
