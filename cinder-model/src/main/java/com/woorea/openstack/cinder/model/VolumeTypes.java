5ca51010c05b9b53e3a9d48bff31db902fedea2c
Daniel Erez <derez@redhat.com>
2014-11-09 17:21:00 +0200

    cinder: introduce client and model
    Cinder - OpenStack Block Storage Service:
    * Added client [1] and model [2] for Volumes / Snapshots / Volume Types.
    * Created a basic functional tests project for verification:
      https://github.com/danielerez/openstack-java-sdk-cinder-tests
    
    [1] client:
    * VolumesExtension:
    - List/Create/UploadToImage/Show/ShowMetadata/
      Delete/Update/Extend/InitializeConnection
    * SnapshotsExtension:
    - List/Create/Show/Delete/Update/ShowMetadata/UpdateMetadata
    * VolumeTypesExtension:
    - List/Create/Show/Delete
    * LimitsExtension:
      List absolute limits.
    
    [2] model:
    * Volumes/Volume/VolumeForCreate/VolumeForExtend/VolumeForUpdate/
      VolumeForImageCreate/ConnectionForInitialize/ConnectionInfo
    * Snapshots/Snapshot/SnapshotForCreate/SnapshotForUpdate
    * VolumeTypes/VolumeType/VolumeTypeForCreate
    * Limits

---
+++ cinder-model/src/main/java/com/woorea/openstack/cinder/model/VolumeTypes.java
@@ -0,0 +1,36 @@
+package com.woorea.openstack.cinder.model;
+
+import java.io.Serializable;
+import java.util.Iterator;
+import java.util.List;
+
+import org.codehaus.jackson.annotate.JsonProperty;
+
+public class VolumeTypes implements Iterable<VolumeType>, Serializable {
+
+    @JsonProperty("volume_types")
+    private List<VolumeType> list;
+
+    /**
+     * @return the list
+     */
+    public List<VolumeType> getList() {
+        return list;
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        return "Volume Types [list=" + list + "]";
+    }
+
+    @Override
+    public Iterator<VolumeType> iterator() {
+        return list.iterator();
+    }
+
+}
