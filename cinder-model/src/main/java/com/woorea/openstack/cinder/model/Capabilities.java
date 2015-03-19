f4a01f2a372b01216390c49f636188fe9b3bfe6f
ansaba <ahmed.nooras.saba@gmail.com>
2015-03-19 11:46:16 -0400

    Added Java SDK for scheduler_stats api in Cinder.
    This extension provides two level of pool information: names only or
    detailed information:
    
    Pool name only:
    GET http://CINDER_API_ENDPOINT/v2/TENANT_ID/scheduler-stats/get_pools
    
    Detailed Pool info:
    GET http://CINDER_API_ENDPOINT/v2/TENANT_ID/scheduler-stats/get_pools
    \?detail\=True
    
    JSON Response
    {
      "pools": [
        {
          "name": "ubuntu@lvm#backend_name",
          "capabilities": {
            "QoS_support": false,
            "allocated_capacity_gb": 0,
            "driver_version": "2.0.0",
            "free_capacity_gb": 7.01,
            "location_info": "LVMVolumeDriver:ubuntu:stack-volumes:default:0",
            "pool_name": "backend_name",
            "reserved_percentage": 0,
            "storage_protocol": "iSCSI",
            "timestamp": "2014-11-21T18:15:28.141161",
            "total_capacity_gb": 10.01,
            "vendor_name": "Open Source",
            "volume_backend_name": "backend_name",
            "extra_specs": [
              {
                "description": "HP 3PAR Fibre Channel driver",
                "display_name": "HP3PARFCDriver",
                "namespace": "OS::Cinder::HP3PARFCDriver",
                "properties": {
                  "hp3par:persona": {
                    "default": "2",
                    "description": "HP 3PAR Persona",
                    "enum": {
                      "1": "Generic",
                      "10": "ONTAP-legacy",
                      "11": "VMware",
                      "12": "OpenVMS",
                      "13": "HPUX",
                      "15": "WindowsServer",
                      "2": "Generic-ALUA",
                      "6": "Generic-legacy",
                      "7": "HPUX-legacy",
                      "8": "AIX-legacy",
                      "9": "EGENERA"
                    },
                    "title": "persona",
                    "type": "string"
                  },
                  "hp3par:snap_cpg": {
                    "description": "Overrides the 'hp3par_snap' setting",
                    "title": "snap_cpg",
                    "type": "string"
                  }
                }
              },
              {
                "description": "HP LeftHand iSCSI driver",
                "display_name": "HPLeftHandISCSIDriver",
                "namespace": "OS::Cinder::HPLeftHandISCSIDriver",
                "properties": {
                  "hplh:provisioning": {
                    "default": "thin",
                    "description": "Provisioning",
                    "enum": [
                      "full",
                      "thin"
                    ],
                    "title": "provisioning",
                    "type": "string"
                  },
                  "hplh:vvs": {
                    "default": "1",
                    "description": "VVS",
                    "title": "vvs",
                    "type": "integer"
                  }
                }
              }
            ]
          }
        }
      ]
    }

---
+++ cinder-model/src/main/java/com/woorea/openstack/cinder/model/Capabilities.java
@@ -0,0 +1,141 @@
+package com.woorea.openstack.cinder.model;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+import org.codehaus.jackson.annotate.JsonProperty;
+import org.codehaus.jackson.map.annotate.JsonRootName;
+
+/**
+ * Model for Volume
+ *
+ * @author Ahmed Saba
+ */
+@JsonRootName("capabilities")
+public class Capabilities implements Serializable {
+
+    @JsonProperty("pool_name")
+    private String poolName;
+    @JsonProperty("QoS_support")
+    private boolean qosSupport;
+    @JsonProperty("allocated_capacity_gb")
+    private Long allocatedCapacityGb;
+    @JsonProperty("driver_version")
+    private String driverVersion;
+    @JsonProperty("free_capacity_gb")
+    private Long freeCapacityGb;
+    @JsonProperty("location_info")
+    private String locationInfo;
+
+    private String timestamp;
+    @JsonProperty("volume_backend_name")
+    private String volumeBackendName;
+    @JsonProperty("total_capacity_gb")
+    private Long totalCapacityGb;
+    @JsonProperty("reserved_percentage")
+    private Integer reservedPercentage;
+    @JsonProperty("vendor_name")
+    private String vendorName;
+    @JsonProperty("storage_protocol")
+    private String storageProtocol;
+    @JsonProperty("extra_specs")
+    private Map<String, String> extraSpecs;
+
+    public String getPoolName() {
+		return poolName;
+	}
+	public void setPoolName(String poolName) {
+		this.poolName = poolName;
+	}
+	public boolean isQosSupport() {
+		return qosSupport;
+	}
+	public void setQosSupport(boolean qosSupport) {
+		this.qosSupport = qosSupport;
+	}
+	public Long getAllocatedCapacityGb() {
+		return allocatedCapacityGb;
+	}
+	public void setAllocatedCapacityGb(Long allocatedCapacityGb) {
+		this.allocatedCapacityGb = allocatedCapacityGb;
+	}
+	public String getDriverVersion() {
+		return driverVersion;
+	}
+	public void setDriverVersion(String driverVersion) {
+		this.driverVersion = driverVersion;
+	}
+	public Long getFreeCapacityGb() {
+		return freeCapacityGb;
+	}
+	public void setFreeCapacityGb(Long freeCapacityGb) {
+		this.freeCapacityGb = freeCapacityGb;
+	}
+	public String getLocationInfo() {
+		return locationInfo;
+	}
+	public void setLocationInfo(String locationInfo) {
+		this.locationInfo = locationInfo;
+	}
+	public String getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(String timestamp) {
+		this.timestamp = timestamp;
+	}
+	public String getVolumeBackendName() {
+		return volumeBackendName;
+	}
+	public void setVolumeBackendName(String volumeBackendName) {
+		this.volumeBackendName = volumeBackendName;
+	}
+	public Long getTotalCapacityGb() {
+		return totalCapacityGb;
+	}
+	public void setTotalCapacityGb(Long totalCapacityGb) {
+		this.totalCapacityGb = totalCapacityGb;
+	}
+	public Integer getReservedPercentage() {
+		return reservedPercentage;
+	}
+	public void setReservedPercentage(Integer reservedPercentage) {
+		this.reservedPercentage = reservedPercentage;
+	}
+	public String getVendorName() {
+		return vendorName;
+	}
+	public void setVendorName(String vendorName) {
+		this.vendorName = vendorName;
+	}
+	public String getStorageProtocol() {
+		return storageProtocol;
+	}
+	public void setStorageProtocol(String storageProtocol) {
+		this.storageProtocol = storageProtocol;
+	}
+	public Map<String, String> getExtraSpecs() {
+		return extraSpecs;
+	}
+	public void setExtraSpecs(Map<String, String> extraSpecs) {
+		this.extraSpecs = extraSpecs;
+	}
+
+    @Override
+    public String toString() {
+        return "Capabilities{"
+                + "poolName='" + poolName + '\''
+                + ", qosSupport='" + qosSupport + '\''
+                + ", allocatedCapacityGb='" + allocatedCapacityGb + '\''
+                + ", driverVersion='" + driverVersion + '\''
+                + ", freeCapacityGb='" + freeCapacityGb + '\''
+                + ", locationInfo='" + locationInfo + '\''
+                + ", timestamp='" + timestamp + '\''
+                + ", volumeBackendName='" + volumeBackendName + '\''
+                + ", totalCapacityGb='" + totalCapacityGb + '\''
+                + ", reservedPercentage='" + reservedPercentage + '\''
+                + ", vendorName='" + vendorName + '\''
+                + ", storageProtocol='" + storageProtocol + '\''
+                + ", extraSpecs='" + extraSpecs + '\''
+                + '}';
+    }
+}
